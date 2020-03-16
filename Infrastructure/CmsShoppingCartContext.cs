a422b70d045d48a2df24dcb80671c35b8dcd6013
Daniil Vatahovych <daniil.vatahovych@iprospect.com>
2020-03-17 00:14:26 +0100

    add Admin area

---
+++ Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,15 @@
+using System;
+using CmsShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext : DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
